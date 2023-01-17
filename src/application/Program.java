63b5383edbc9f59cffa13dbce185e16d539abc76
Thierry Marinho <thierrydev48@gmail.com>
2023-01-17 08:36:48 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,8 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.dao.impl.SellerDaoJDBC;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,6 +15,8 @@ public class Program {
         Department department = new Department(1, "Books");
 
         Seller seller = new Seller(13, "Thierry", "thierry@gmail.com", new Date(), 3000.0, department);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println(seller);
     }
--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -4,7 +4,7 @@ import model.entities.Seller;
 
 import java.util.List;
 
-public interface Sellerdao {
+public interface SellerDao {
 
     void insert(Seller obj);
     void update(Seller obj);
